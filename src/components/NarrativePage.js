db827f4157315a93f283b7191bb87a32f75905e3
Kenneth Nguyen <kennethnguyen007@gmail.com>
2024-03-06 12:14:37 -0500

    Added content to narrative page

--- src/components/NarrativePage.js
+++ src/components/NarrativePage.js
@@ -1,9 +1,13 @@
 import React from 'react';
+import ISeeYouStillImage from '../assets/images/ISeeYouStill.jpg'; // Import your image
 
 function NarrativePage() {
   return (
-    <div style={{ textAlign: 'center', marginTop: '100px', fontSize: '36px', fontFamily: 'Futura, sans-serif', marginBottom: '450px' }}>
-      COMING SPRING 2024.
+    <div style={{ textAlign: 'center', marginTop: '30px', fontFamily: 'Futura, sans-serif' }}>
+      <div style={{ fontSize: '30px', marginBottom: '10px' }}>
+        COMING SPRING 2024.
+      </div>
+      <img src={ISeeYouStillImage} alt="ISeeYouStill" style={{ maxWidth: '80%', maxHeight: 'auto', width: 'auto', height: 'auto', display: 'block', margin: '0 auto' }} />
     </div>
   );
 }
