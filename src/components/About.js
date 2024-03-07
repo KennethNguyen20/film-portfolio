2fc06f16c3cb6afb4f623eb92e944bcf628326a3
Kenneth Nguyen <kennethnguyen007@gmail.com>
2024-03-07 17:23:38 -0500

    Updated about page

--- src/components/About.js
+++ src/components/About.js
@@ -20,7 +20,7 @@ function About() {
         <a href={resumePDF} target="_blank" rel="noopener noreferrer">View Resume</a>
       </div>
       <div className="about-content">
-        <p>Kenneth Nguyen is a cinematographer from Herndon, Virginia. He is currently attending the University of Virginia pursuing a double major in Computer Science and Cinematography. With a diverse portfolio, he showcases his talent as a videographer for VMagazine and The Moth: MainStage at Paramount Theaters, and as Director of Photography for the anticipated short film "I See You," set to premiere at the Overcranked Film Festival in Spring 2024. Proficient in both film and digital mediums, Kenneth is eager to find new opportunities in filmmaking.</p>
+        <p>Kenneth Nguyen is a cinematographer from Herndon, Virginia. He currently attends the University of Virginia pursuing a double major in Computer Science and Cinematography. With a diverse portfolio, he showcases his talent as a videographer for VMagazine and The Moth: MainStage at Paramount Theaters, and as Director of Photography for the anticipated short film "I See You," set to premiere at the Overcranked Film Festival in Spring 2024. Proficient in both film and digital mediums, Kenneth is eager to find new opportunities in filmmaking.</p>
       </div>
     </div>
   );
