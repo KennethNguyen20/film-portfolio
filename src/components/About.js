cfad803df40559b6487b16fe89b006456d64ad0f
Kenneth Nguyen <kennethnguyen007@gmail.com>
2024-07-29 09:17:45 -0400

    Removed resume button

--- src/components/About.js
+++ src/components/About.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import aboutPic from '../assets/images/aboutPic.JPG'; 
 import bolexPic from '../assets/images/bolex.jpg'; 
-import resumePDF from '../assets/files/NguyenKennethFilmResume.pdf'; 
 import './About.css'; 
 
 function About() {
@@ -17,7 +16,6 @@ function About() {
       </div>
       <div className="about-links">
         <a href="mailto:kennethknguyen7@gmail.com">Email: kennethknguyen7@gmail.com</a>
-        <a href={resumePDF} target="_blank" rel="noopener noreferrer">View Resume</a>
       </div>
       <div className="about-content">
         <p>Kenneth Nguyen is a Director of Photography/Cinematographer from Herndon, Virginia. He currently attends the University of Virginia pursuing a double major in Computer Science and Cinematography. With a diverse portfolio, he showcases his talent working with Shelley Whitman Associates, VMagazine, Jetsweat Fitness, and The Moth: MainStage at Paramount Theater. His short films have gained recognition at international film festivals, and he is actively working on new projects. Proficient in both film and digital mediums, Kenneth is eager to find new opportunities in filmmaking.</p>
