6018e9ea6eb3b5ce400f14e3aa2c5848485f2f63
Kenneth Nguyen <kennethnguyen007@gmail.com>
2025-03-18 15:55:20 +0100

    Updated about biography with VMFA fellowship

--- src/components/About.js
+++ src/components/About.js
@@ -18,7 +18,22 @@ function About() {
         <a href="mailto:kennethknguyen7@gmail.com">Email: kennethknguyen7@gmail.com</a>
       </div>
       <div className="about-content">
-        <p>Kenneth Nguyen is a filmmaker/director from Herndon, Virginia. He currently attends the University of Virginia pursuing a double major in Film and Computer Science. With a diverse portfolio, he showcases his talent working with the Black & Indigenous Feminist Features Insitute, Shelley Whitman Associates, VMagazine, UVA Karsh Institute, and The Moth: MainStage at Paramount Theater. His short films have gained recognition at international film festivals, and he is actively working on new projects. Proficient in both film and digital mediums, Kenneth is eager to find new opportunities in filmmaking.</p>
+        <h3>ABOUT</h3>
+        <p>
+          Kenneth Nguyen is a filmmaker and director based in Virginia, and a Virginia Museum of Fine Arts (VMFA) Fellow. 
+          He currently attends the University of Virginia, pursuing a double major in Film and Computer Science. 
+          With a diverse portfolio, he showcases his talent working with the Black & Indigenous Feminist Features Institute, 
+          Shelley Whitman Associates, VMagazine, UVA Karsh Institute, and The Moth: MainStage at Paramount Theater. 
+          His short films have gained recognition at international film festivals, and he is actively working on new projects. 
+          Proficient in both film and digital mediums, Kenneth is eager to find new opportunities in filmmaking.
+        </p>
+
+        <h3>AWARDS</h3>
+        
+          <p>Virginia Museum of Fine Arts Fellowship 2025-2026</p>
+          <p>Tokyo International Short Film Festival 2024, <em>Official Selection</em> for “I See You”</p>
+          <p>Cinéma Royale International Film Festival 2024, <em>Official Selection</em> for “I See You”</p>
+        
       </div>
     </div>
   );
